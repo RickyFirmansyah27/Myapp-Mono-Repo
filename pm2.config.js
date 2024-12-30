e090efbd34ebb6e22ff5f78e1151e12d5ba0f257
Ricky Firmansyah <rickyfirmansyah2707@gmail.com>
2024-12-30 11:17:48 +0700

    add logger pm2

--- pm2.config.js
+++ pm2.config.js
@@ -1,4 +1,4 @@
-//PM2 Config
+// PM2 Config
 module.exports = {
   apps: [
     {
@@ -6,36 +6,55 @@ module.exports = {
       script: "make",
       args: "run",
       cwd: "apps/actix-service",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      output: "logs/actix-service-out.log",  // Log output
+      error: "logs/actix-service-error.log", // Log error
     },
     {
       name: "django-service",
       script: "make",
       args: "run",
       cwd: "apps/django-service",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      output: "logs/django-service-out.log",  // Log output
+      error: "logs/django-service-error.log", // Log error
     },
     {
       name: "dotnet-service",
       script: "make",
       args: "run",
       cwd: "apps/dotnet-service",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      output: "logs/dotnet-service-out.log",  // Log output
+      error: "logs/dotnet-service-error.log", // Log error
     },
     {
       name: "fiber-service",
       script: "make",
       args: "run",
       cwd: "apps/fiber-service",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      output: "logs/fiber-service-out.log",  // Log output
+      error: "logs/fiber-service-error.log", // Log error
     },
-    {
-      name: "hyper-service",
-      script: "make",
-      args: "run",
-      cwd: "apps/hyper-service",
-    },
+    // issue looping pm2 run nodemon
+    // {
+    //   name: "hyper-service",
+    //   script: "make",
+    //   args: "run",
+    //   cwd: "apps/hyper-service",
+    //   log_date_format: "YYYY-MM-DD HH:mm:ss",
+    //   output: "logs/hyper-service-out.log",  // Log output
+    //   error: "logs/hyper-service-error.log", // Log error
+    // },
     {
       name: "vertx-service",
       script: "make",
       args: "run",
       cwd: "apps/vertx-service",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      output: "logs/vertx-service-out.log",  // Log output
+      error: "logs/vertx-service-error.log", // Log error
     },
   ],
 };
